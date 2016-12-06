8fb44b3054857003153af17ac07e5a44e40783c0
Spencer Hensel <its@notlikely.me>
2016-12-06 12:41:53 -0500

    Some Changes
    Check them out?

---
+++ Omnitoo/Assets/Scripts/gem_rotator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gem_rotator : MonoBehaviour {
+	public float rotation_speed = -20f;
+	private float change_timer = 10f;
+	private float cur_time = 0;
+	private float set_speed = 0;
+	// Use this for initialization
+	void Start () {
+		set_speed = Random.Range (-180, -40);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (transform.forward * rotation_speed * Time.deltaTime);
+		if (Mathf.Abs(set_speed - rotation_speed) > 1) {
+			rotation_speed = Mathf.Lerp (rotation_speed, set_speed, Time.deltaTime / 2);
+			cur_time += Time.deltaTime;
+		} else {
+			set_speed = Random.Range (-180, -40);
+		}
+	}
+}
--- Omnitoo/Assets/Scripts/player_movement.cs
+++ Omnitoo/Assets/Scripts/player_movement.cs
@@ -7,7 +7,7 @@ public class player_movement : MonoBehaviour {
 	public GameObject thing;
 	public GameObject gem;
 	public GameObject gem2;
-	float[] gemRot;
+	public GameObject spinner;
 
 
 
@@ -15,24 +15,16 @@ public class player_movement : MonoBehaviour {
 	private Vector3 heading;
 	private float magnitude;
 	private float startSize;
-	[Header("Gem Settings")]
-	public int gemLayers = 0;
-	public float gemSkin = .5f;
-	public float gemDensity = .5f;
-	public float gemStartDistance = 5f;
-	public Sprite gemSprite;
-	
+
 
 	void Start () {
 		startSize = Camera.main.orthographicSize;
 		genSpikes (3,true);
-		//genGems (9, 2);
-		//genGems (15, 3);
-		//genGems (20, 4);
-		//genGems (30, 5);
-		//genGems (40, 6);
-		gemRot = new float[2];
-		genGems(gemLayers);
+		genGems (10, 2, gem2);
+		genGems (15, 3, gem);
+		genGems (20, 4, gem2);
+		genGems (30, 5, gem);
+
 	}
 
 	void Update () {
@@ -49,83 +41,24 @@ public class player_movement : MonoBehaviour {
 		}
 	
 	}
-	
-	void genGems(int layer)
-	{
-		float extents = 2 * gemSprite.bounds.extents.y;
-		float theta = 0f;
-		float x;
-		float y;
-		float delta;
-		int count;
 
-		GameObject temp;
-		for (int j = 1; j <= layer; j++)
-		{
-			if (j == 1)
-			{
-
-				temp = Instantiate(gem) as GameObject;
-				gemRot[0] = 0f;
-				temp.transform.position = new Vector2(transform.position.x + gemStartDistance, transform.position.y);
-				temp.transform.SetParent(transform);
-
-				temp = Instantiate(gem) as GameObject;
-				gemRot[1] = 2 * Mathf.PI / 3;
-				x = Mathf.Cos(gemRot[1]);
-				y = Mathf.Sin(gemRot[1]);
-				temp.transform.position = new Vector2(transform.position.x + x * gemStartDistance, transform.position.y + y * gemStartDistance);
-				temp.transform.SetParent(transform);
-
-
-				x = Mathf.Cos(4 * Mathf.PI / 3);
-				y = Mathf.Sin(4 * Mathf.PI / 3);
-				temp = Instantiate(gem, new Vector2(x * gemStartDistance + transform.position.x, y * gemStartDistance + transform.position.y), Quaternion.identity) as GameObject;
-				temp.transform.SetParent(transform);
-			}
-			else
-			{
-				delta = gemRot[1] - gemRot[0];
-				
-				float r = j * (gemSkin + extents) - extents / 2 + gemStartDistance;
-				count = (int)((2 * Mathf.PI * r) / extents * .5);
-				float tempTheta = delta / 2;
-				theta = delta / 2;
-				for (int i = 0; i < count; i++)
-				{
-					temp = Instantiate(gem) as GameObject;
-
-					theta = i * 2 * Mathf.PI / count + tempTheta;
-
-					if (i == 0)
-						gemRot[0] = theta;
-					else if (i == 1)
-						gemRot[1] = theta;
-
-					x = Mathf.Cos(theta);
-					y = Mathf.Sin(theta);
-
-					temp.transform.position = new Vector2(x * r + transform.position.x, y * r + transform.position.y);
-					temp.transform.SetParent(transform);
-				}
-			}
-		}
-	}
 		
-	void genGems(int gems, int layer){
+	void genGems(int gems, int layer, GameObject gemmy){
+		GameObject spinny = Instantiate (spinner, transform.position, Quaternion.identity) as GameObject;
+		spinny.transform.SetParent (transform);
+		spinny.GetComponent<gem_rotator> ().rotation_speed = Random.Range (-180, -40);
 		int offset = 0;
 		if (layer % 2 == 0) {
 			offset = 0;
 		}
 		for (int i = 0; i < gems; i++) {
-			GameObject test = Instantiate (gem2, transform.position + fromAngle (layer * 2.25f, (360 / gems * i) + offset), Quaternion.identity) as GameObject;
-			test.transform.LookAt (transform.position);
+			GameObject test = Instantiate (gemmy, transform.position + fromAngle (layer * 2.25f, (360 / gems * i) + offset), Quaternion.identity) as GameObject;
 			Vector3 diff = transform.position - test.transform.position;
 			diff.Normalize();
 
 			float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
 			test.transform.rotation = Quaternion.Euler(0f, 0f, rot_z + 90);
-			test.transform.SetParent (transform);
+			test.transform.SetParent (spinny.transform);
 		}
 	}
 
